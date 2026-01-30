dff6dff70fbeac60adbf0023e9d806bf9267549b
LuWo0 <loviedo46@yahoo.com>
2026-01-29 19:28:06 -0800

    Created simple red sphere

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,20 @@
 #include "../include/vec3.hpp"
 #include <iostream>
 
+bool hit_sphere(const Point3 &center, double radius, const Ray &r) {
+  Vec3 oc{center - r.origin()};
+  auto a{dot(r.direction(), r.direction())};
+  auto b{-1.0 * 2.0 * dot(r.direction(), oc)};
+  auto c{dot(oc, oc) - radius * radius};
+  auto discriminant{b * b - 4 * a * c};
+  return (discriminant >= 0);
+}
+
 Color ray_color(const Ray &r) {
+  if (hit_sphere(Point3(0, 0, -1), 0.5, r)) {
+    return Color(1, 0, 0);
+  }
+
   Vec3 unit_direction{unit_vector(r.direction())};
   double a{0.5 * (unit_direction.y() + 1.0)};
   return (1.0 - a) * Color(1.0, 1.0, 1.0) + a * Color(0.5, 0.7, 1.0);
